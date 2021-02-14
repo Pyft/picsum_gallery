d6b1fa3aebee917b9416e8704da0bc7a13a075d3
YAP <pya95@bk.ru>
2021-02-14 23:30:12 +0300

    Update two_colomn_gallery.dart

--- lib/two_colomn_gallery.dart
+++ lib/two_colomn_gallery.dart
@@ -6,12 +6,13 @@ class TwoColomnGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Picsum gallery',
-        theme: ThemeData(
-          primaryColor: Color.fromRGBO(160, 190, 190, 1),
-          textTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
-          accentColor: Colors.amber,
-        ),
-        home: HomePage());
+      title: 'Picsum gallery',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(160, 190, 190, 1),
+        textTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
+        accentColor: Colors.amber,
+      ),
+      home: HomePage(),
+    );
   }
 }
